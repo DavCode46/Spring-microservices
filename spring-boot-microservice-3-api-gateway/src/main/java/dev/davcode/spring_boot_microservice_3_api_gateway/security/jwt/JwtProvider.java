00b65151a933c41884da946c2bffddec1a5c66c6
Davidmb <davidblanco1993@gmail.com>
2025-04-14 22:32:26 +0200

    feat: jwt provider done

---
+++ spring-boot-microservice-3-api-gateway/src/main/java/dev/davcode/spring_boot_microservice_3_api_gateway/security/jwt/JwtProvider.java
@@ -0,0 +1,15 @@
+package dev.davcode.spring_boot_microservice_3_api_gateway.security.jwt;
+
+import dev.davcode.spring_boot_microservice_3_api_gateway.security.UserPrincipal;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
+public interface JwtProvider {
+
+
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean isTokenValid(HttpServletRequest request);
+}
--- spring-boot-microservice-3-api-gateway/src/main/java/dev/davcode/spring_boot_microservice_3_api_gateway/utils/SecurityUtils.java
+++ spring-boot-microservice-3-api-gateway/src/main/java/dev/davcode/spring_boot_microservice_3_api_gateway/utils/SecurityUtils.java
@@ -1,14 +1,29 @@
 package dev.davcode.spring_boot_microservice_3_api_gateway.utils;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.util.StringUtils;
 
 public class SecurityUtils {
 
     public static final String ROLE_PREFIX = "Role_";
+    public static final String AUTH_HEADER = "authorization";
+    public static final String AUTH_TOKEN_TYPE = "Bearer";
+    public static final String AUTH_TOKEN_PREFIX = AUTH_TOKEN_TYPE + " ";
 
     public static SimpleGrantedAuthority convertToAuthority(String role) {
         String formattedRole = role.startsWith(ROLE_PREFIX) ? role : ROLE_PREFIX + role;
         return new SimpleGrantedAuthority(formattedRole);
+    }
+
+    public static String extractAuthtoTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTH_HEADER);
+
+        if(StringUtils.hasLength(bearerToken) && bearerToken.startsWith(AUTH_TOKEN_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
     }
 
 }
