6ff0da7a5d23e48dc9236df40aeaa9928d0e91e1
Davidmb <davidblanco1993@gmail.com>
2025-04-14 21:41:21 +0200

    feat security done

--- spring-boot-microservice-3-api-gateway/src/main/java/dev/davcode/spring_boot_microservice_3_api_gateway/security/CustomUserDetailsService.java
+++ spring-boot-microservice-3-api-gateway/src/main/java/dev/davcode/spring_boot_microservice_3_api_gateway/security/CustomUserDetailsService.java
@@ -8,15 +8,16 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserService userService;
-    
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
---
+++ spring-boot-microservice-3-api-gateway/src/main/java/dev/davcode/spring_boot_microservice_3_api_gateway/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package dev.davcode.spring_boot_microservice_3_api_gateway.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder auth = http.getSharedObject(
+                AuthenticationManagerBuilder.class
+        );
+
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder);
+
+        AuthenticationManager authenticationManager = auth.build();
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/authentication/sign-in", "/api/authentication/sign-up").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
