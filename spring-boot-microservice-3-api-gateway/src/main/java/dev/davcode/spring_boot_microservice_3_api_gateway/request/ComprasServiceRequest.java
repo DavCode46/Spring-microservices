0f686f056f9177a0122e7fb2789aa93a45e7f4e7
Davidmb <davidblanco1993@gmail.com>
2025-04-15 20:23:12 +0200

    feat: eureka config done

--- spring-boot-microservice-3-api-gateway/src/main/java/dev/davcode/spring_boot_microservice_3_api_gateway/request/ComprasServiceRequest.java
+++ spring-boot-microservice-3-api-gateway/src/main/java/dev/davcode/spring_boot_microservice_3_api_gateway/request/ComprasServiceRequest.java
@@ -8,7 +8,7 @@ import java.util.List;
 @FeignClient(
         value="compra-service",
         path = "/api/compra",
-        url = "${compras.service.url}",
+        //url = "${compras.service.url}",
         configuration = FeignConfiguration.class
 )
 public interface ComprasServiceRequest {
